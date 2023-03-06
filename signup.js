1547a2bb7cc8f2f3afce37de9bc80f58e1dab6de
Hien Vu <vuxuanhien.2311@gmail.com>
2023-03-06 15:29:49 +0700

    first commit

---
+++ signup.js
@@ -0,0 +1,40 @@
+
+function postData() {
+  const url = "https://localhost:64851/api/add";
+  const data = {fullName: "Test FInal", phoneNumber: "09999999999",location: "Ha Noi", passwordHash: "Hien1998@",role: "client"};
+  const sendUser = {fullName: document.getElementById("fullName").value,
+   phoneNumber: document.getElementById("phoneNumber").value,
+   location: document.getElementById("location").value,
+   passwordHash:document.getElementById("passwordHash").value,
+   role: "Client"};
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(sendUser),
+  };
+  
+  fetch(url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error("Error: " + response.status);
+      }
+    })
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+var check = function()
+{
+  if(document.getElementById("passwordHash").value == document.getElementById("repeatPass").value){
+    document.getElementById("repeatpass").style.color = 'red';
+    document.getElementById("repeatpass").innerHTML = 'matching';
+  }else{
+    document.getElementById("repeatpass").style.color = 'red';
+    document.getElementById("repeatpass").innerHTML = 'not matching';
+  }
+}
