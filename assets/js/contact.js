fd2c208be8072f00e058f822be719b669091cc96
Hien Vu <vuxuanhien.2311@gmail.com>
2023-03-30 15:56:45 +0700

    fix contact func and check environment

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,7 +4,14 @@ const phoneNum = document.getElementById('phoneNum');
 const mess = document.getElementById('mess');
 const submit = document.getElementById('submit');
 
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
 submit.addEventListener('submit',(e)=>{
+    showSpinner();
+
     e.preventDefault();
     let ebody = `
     <h1>Họ tên: </h1>${fname.value}
@@ -12,7 +19,6 @@ submit.addEventListener('submit',(e)=>{
     <h1>Số đt: </h1>${phoneNum.value}
     <br>
     <h1>Tin nhắn: </h1>${mess.value}
-
     `;
 
     Email.send({
@@ -22,6 +28,46 @@ submit.addEventListener('submit',(e)=>{
         Subject : "ClientContact",
         Body : ebody
     }).then(
-      message => alert(message)
+      fname.value = "",
+      phoneNum.value ="",
+      mess.value= "",
+      delay(3000).then(() => hideSpinner())
     );
+
+    
+     
 });
+
+
+function showSpinner(second) 
+{
+  var overlay = document.getElementById('overlay');
+  var showSpinner = document.getElementById('spinner');
+  overlay.classList.remove('hidden');
+  showSpinner.classList.remove('hidden');
+  
+  // await delay(5000);
+}
+function hideSpinner()
+{
+  var showSpinner = document.getElementById('spinner');
+  showSpinner.classList.add('hidden');
+  showModal();
+}
+function showModal()
+{
+// var openButton = document.getElementById('open');
+    var dialog = document.getElementById('dialog');
+    var closeButton = document.getElementById('close');
+    var overlay = document.getElementById('overlay');
+
+    // show the overlay and the dialog
+      dialog.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+
+    // hide the overlay and the dialog
+    closeButton.addEventListener('click', function () {
+      dialog.classList.add('hidden');
+      overlay.classList.add('hidden');
+    });
+}
